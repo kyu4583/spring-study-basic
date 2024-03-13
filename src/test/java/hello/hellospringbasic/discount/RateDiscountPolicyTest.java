78ab3f29408b1b97f54b73fa7d925b675fd5d259
신성규 <kxu4583@naver.com>
2024-03-13 13:40:33 +0900

    test: 정률 할인 정책 테스트 구현

---
+++ src/test/java/hello/hellospringbasic/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package hello.hellospringbasic.discount;
+
+import hello.hellospringbasic.member.Grade;
+import hello.hellospringbasic.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        String memberName = "memberVIP";
+        int price = 10000;
+        // when
+        int discount = discountPolicy.discount(member, price);
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        // given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        String memberName = "memberBASIC";
+        int price = 10000;
+        // when
+        int discount = discountPolicy.discount(member, price);
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}
