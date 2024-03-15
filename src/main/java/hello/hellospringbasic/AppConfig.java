5584da2f85af9cc87decef42f43a4eee00c954ee
신성규 <kxu4583@naver.com>
2024-03-15 17:26:03 +0900

    feat: 정률 할인 정책으로 변경

--- src/main/java/hello/hellospringbasic/AppConfig.java
+++ src/main/java/hello/hellospringbasic/AppConfig.java
@@ -2,6 +2,7 @@ package hello.hellospringbasic;
 
 import hello.hellospringbasic.discount.DiscountPolicy;
 import hello.hellospringbasic.discount.FixDiscountPolicy;
+import hello.hellospringbasic.discount.RateDiscountPolicy;
 import hello.hellospringbasic.member.MemberRepository;
 import hello.hellospringbasic.member.MemberService;
 import hello.hellospringbasic.member.MemberServiceImpl;
@@ -23,7 +24,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
