bab6f4a4cbc2d381606be1d2d586e85bbea8ea9f
신성규 <kxu4583@naver.com>
2024-03-15 17:19:17 +0900

    test: AppConfig 적용

--- src/test/java/hello/hellospringbasic/order/OrderServiceTest.java
+++ src/test/java/hello/hellospringbasic/order/OrderServiceTest.java
@@ -1,18 +1,26 @@
 package hello.hellospringbasic.order;
 
+import hello.hellospringbasic.AppConfig;
 import hello.hellospringbasic.member.Grade;
 import hello.hellospringbasic.member.Member;
 import hello.hellospringbasic.member.MemberService;
 import hello.hellospringbasic.member.MemberServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
 
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
     @Test
     void createOrder() {
         Long memberId = 1L;
