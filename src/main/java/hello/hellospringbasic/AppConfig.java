45b55a01f82d4daf45dbbf25dcf8412da817f6b9
신성규 <kxu4583@naver.com>
2024-03-15 17:21:30 +0900

    refactor: AppConfig 리팩토링
    앱 전체의 구성 정보를 표현하는 형태로 리팩토링

--- src/main/java/hello/hellospringbasic/AppConfig.java
+++ src/main/java/hello/hellospringbasic/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospringbasic;
 
+import hello.hellospringbasic.discount.DiscountPolicy;
 import hello.hellospringbasic.discount.FixDiscountPolicy;
 import hello.hellospringbasic.member.MemberRepository;
 import hello.hellospringbasic.member.MemberService;
@@ -10,11 +11,19 @@ import hello.hellospringbasic.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
